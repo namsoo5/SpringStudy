eebc3cab86b9cd0df62af36f3b77fea3f6b3a381
namsoo5 <are34@naver.com>
2019-11-09 12:10:35 +0900

    빼기 나누기 곱하기

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,34 @@ public class HelloController {
     @GetMapping("/hello")
     public String Hello(){
         return "Hello, Spring!";
+    }
+
+    //get /sum/?num=[]&num2=[]
+    @GetMapping("/sum")
+    public int Sum(@RequestParam("num1") int a, @RequestParam("num2") int b){
+
+        return a+b;
+
+    }
+
+    @GetMapping("/multi")
+    public int Mutli(@RequestParam("num1") int a, @RequestParam("num2") int b){
+
+        return a*b;
+
+    }
+
+    @GetMapping("/div")
+    public float Div(@RequestParam("num1") int a, @RequestParam("num2") int b){
+
+        return (float)a/b;
+
+    }
+
+    @GetMapping("/sub")
+    public int Sub(@RequestParam("num1") int a, @RequestParam("num2") int b){
+
+        return a-b;
+
     }
 }
