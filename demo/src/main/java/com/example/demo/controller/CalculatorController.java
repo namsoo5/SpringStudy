90422ed01986340cfa0957d13cab298854ba4170
namsoo5 <are34@naver.com>
2019-11-10 14:42:34 +0900

    과제 - 계산 확장기능 구현

--- demo/src/main/java/com/example/demo/controller/CalculatorController.java
+++ demo/src/main/java/com/example/demo/controller/CalculatorController.java
@@ -21,4 +21,40 @@ public class CalculatorController {
         return calculatorService.Sum(a,b);
     }
 
+    @GetMapping("/sub")
+    public int Sub(@RequestParam("num1") int a, @RequestParam("num2") int b){
+        return calculatorService.Sub(a,b);
+    }
+
+    @GetMapping("/div")
+    public float Div(@RequestParam("num1") int a, @RequestParam("num2") int b){
+        return calculatorService.Div(a,b);
+    }
+
+    @GetMapping("/multi")
+    public int Multi(@RequestParam("num1") int a, @RequestParam("num2") int b){
+        return calculatorService.Mutli(a,b);
+    }
+
+    @GetMapping("/mod")
+    public float Mod(@RequestParam("num1") int a, @RequestParam("num2") int b){
+        return calculatorService.Mod(a,b);
+    }
+
+    @GetMapping("/min")
+    public int Min(@RequestParam("num1") int a, @RequestParam("num2") int b){
+        return calculatorService.Min(a,b);
+    }
+
+    @GetMapping("/max")
+    public int Max(@RequestParam("num1") int a, @RequestParam("num2") int b){
+        return calculatorService.Max(a,b);
+    }
+
+    @GetMapping("/pow")
+    public double Pow(@RequestParam("num1") int a, @RequestParam("num2") int b){
+        return calculatorService.Pow(a,b);
+    }
+
+
 }
--- demo/src/main/java/com/example/demo/service/CalculatorService.java
+++ demo/src/main/java/com/example/demo/service/CalculatorService.java
@@ -7,31 +7,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CalculatorService {
 
     //get /sum/?num=[]&num2=[]
-    @GetMapping("/sum")
+//    @GetMapping("/sum")
     public int Sum(int a, int b){
 
         return a+b;
-
     }
 
-    @GetMapping("/multi")
+//    @GetMapping("/multi")
     public int Mutli(int a, int b){
 
         return a*b;
-
     }
 
-    @GetMapping("/div")
+//    @GetMapping("/div")
     public float Div(int a, int b){
 
         return (float)a/b;
-
     }
 
-    @GetMapping("/sub")
+//    @GetMapping("/sub")
     public int Sub(int a, int b){
 
         return a-b;
+    }
 
+//    @GetMapping("/mod")
+    public float Mod(int a, int b){
+
+        return a%b;
+    }
+
+//    @GetMapping("/min")
+    public int Min(int a, int b){
+
+        return Math.min(a,b);
+    }
+
+//    @GetMapping("/max")
+    public int Max(int a, int b){
+
+        return Math.max(a,b);
+    }
+
+//    @GetMapping("/pow")
+    public double Pow(int a, int b){
+
+        return Math.pow(a,b);
     }
 }
